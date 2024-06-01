43cfdac5978c1a3ee3d23f79ccc1c3bd99830886
K2 <jinklolfunny1@gmail.com>
2024-06-01 22:15:26 +0700

    Update checkout form

--- src/app/admin/actions.ts
+++ src/app/admin/actions.ts
@@ -37,3 +37,23 @@ export async function getProductData() {
 
     return { activeCount, inactiveCount }
 }
+
+export async function createDownloadVerification(productId: string) {
+    return (
+        await db.downloadVerification.create({
+            data: {
+                productId,
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+            },
+        })
+    ).id
+}
+
+export async function userOrderExists(email: string, productId: string) {
+    return (
+        (await db.order.findFirst({
+            where: { user: { email }, productId },
+            select: { id: true },
+        })) != null
+    )
+}
